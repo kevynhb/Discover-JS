873d08fc94c6b5e245e84700423c2627826b5262
Kevyn Hotts <kevynh36@gmail.com>
2021-09-17 16:46:30 -0400

    first commit

---
+++ Tipos de dados/string.js
@@ -0,0 +1,14 @@
+// Aspas duplas e aspas simples
+console.log("posso escrever algo com 'dentro' sem ter problemas")
+console.log('O mesmo vale para o "contrario"')
+
+// Utilizando craze - template literals ou template strings
+console.log(`
+kevyn
+permite multiplas linhas
+`)
+4
+// Permite Interpolação ${...}
+console.log(`permite expressões de linguagem
+${1 + 1} 
+`)
---
+++ scripts.js
@@ -0,0 +1,13 @@
+console.log("posso escrever algo com 'dentro' sem ter problemas")
+console.log('O mesmo vale para o "contrario"')
+
+// Utilizando craze
+console.log(`
+kevyn
+permite multiplas linha
+`)
+4
+// Interpolação
+console.log(`permite expressões de linguagem
+${1 + 1} INTERPOLAÇÃO
+`)
