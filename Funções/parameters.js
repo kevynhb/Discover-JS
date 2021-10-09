87857a59c17d6c91ca028ef1b9562d4f119b3873
Kevyn Hotts <kevynh36@gmail.com>
2021-10-08 21:21:14 -0400

    add functions

---
+++ Funções/parameters.js
@@ -0,0 +1,11 @@
+// functino expression - anonymous
+
+// oarâmetros - parameters
+const sum = function (number1, number2) {
+  res = number1 + number2
+  console.log(number1 + number2)
+}
+
+sum(2, 3) // arguments
+
+console.log(res)
